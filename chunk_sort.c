0515dcc63abc1d080da7465891e7bb5476d8a8ee
ZeynepMahinur <122391696+ZeynepMahinur@users.noreply.github.com>
2025-10-14 18:38:17 +0300

    Add files via upload

--- chunk_sort.c
+++ chunk_sort.c
@@ -21,18 +21,25 @@ void    chunk_sort(t_stack **stack_a, t_stack **stack_b)
     int size;
     int max;
     int chunk_size;
+    int chunk_count;
 
     index_stack(*stack_a);
     size = stack_size(*stack_a);
-    if (size <= 10)
-        chunk_size = 2;
-    else if (size <= 60)
-        chunk_size = 10;
+    if (size <= 5)
+        chunk_count = 1;
+    else if (size <= 10)
+        chunk_count = 2;
     else
-        chunk_size = size / 6;
+        chunk_count = 6;
+    chunk_size = size / chunk_count;
     max = chunk_size;
     while (*stack_a)
     {
+        if (size <= 5 && stack_size(*stack_a) <= 3)
+        {
+            sort_3(stack_a);
+            break;
+        }
         if ((*stack_a)->index < max)
         {
             pb(stack_a, stack_b);
@@ -46,9 +53,10 @@ void    chunk_sort(t_stack **stack_a, t_stack **stack_b)
     }
     while (*stack_b)
     {
-        if ((*stack_b)->index == max_index(*stack_b))
-            pa(stack_a, stack_b);
-        else
+        if ((*stack_b)->index != max_index(*stack_b))
             rb(stack_b);
+        pa(stack_a, stack_b);
     }
+    if ((*stack_a)->next_node && (*stack_a)->number > (*stack_a)->next_node->number)
+        sa(stack_a);
 }
